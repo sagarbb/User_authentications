c34893d7a5f064e79f0c00df49e0275ff2a069cc
sagarbb <bpremasagar5143@gmail.com>
2024-04-29 18:57:07 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
-    username= forms.CharField()
-    password= forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField()
+    password =  forms.CharField(widget=forms.PasswordInput)
 
 
 class UserRegistrationForm(forms.ModelForm):
